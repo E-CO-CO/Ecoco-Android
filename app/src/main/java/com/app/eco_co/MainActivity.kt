dd85b1458cca6d1e7abfb89c2953c430964a2418
chang0-0 <bigyoung8375@naver.com>
2023-08-18 15:42:00 +0900

    initial commit

---
+++ app/src/main/java/com/app/eco_co/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.app.eco_co
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
