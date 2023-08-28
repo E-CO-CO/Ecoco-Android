c72ab5fc4e61d133457aef27883da7e8f371ee9a
chang0-0 <bigyoung8375@naver.com>
2023-08-28 12:58:31 +0900

    #3 [CHORE] ic_arrow 수정 요청, next_green_button 커스텀 뷰 텍스트 수정 요청 수정 완료

--- app/src/main/java/com/app/eco_co/common/view/start/UserInformInputFragment.kt
+++ app/src/main/java/com/app/eco_co/common/view/start/UserInformInputFragment.kt
@@ -2,9 +2,9 @@ package com.app.eco_co.common.view.start
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import com.app.eco_co.common.util.BaseFragment
+import com.app.eco_co.common.view.custom.NextGreenButtonCustomView
 import com.app.eco_co.databinding.FragmentUserInformInputBinding
 
 private const val TAG = "UserInformInputFragment_창영"
@@ -12,6 +12,7 @@ private const val TAG = "UserInformInputFragment_창영"
 class UserInformInputFragment :
     BaseFragment<FragmentUserInformInputBinding>(FragmentUserInformInputBinding::inflate) {
     private lateinit var mContext: Context
+    private lateinit var nextGreenButton: NextGreenButtonCustomView
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -20,7 +21,9 @@ class UserInformInputFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d(TAG, "onViewCreated: ")
+        nextGreenButton = binding.btnUserInformInputNext
+        nextGreenButton.setView(1)
+
 
     } // End of onViewCreated
 } // End of UserInformInputFragment class
