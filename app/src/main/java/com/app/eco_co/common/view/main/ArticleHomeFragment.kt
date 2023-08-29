44bc105c27f501eab6ae4d0cb65a90d65992594b
chang0-0 <bigyoung8375@naver.com>
2023-08-29 11:55:19 +0900

    #3 [FEAT] 아티클 레이아웃 개발 init push

---
+++ app/src/main/java/com/app/eco_co/common/view/main/ArticleHomeFragment.kt
@@ -0,0 +1,20 @@
+package com.app.eco_co.common.view.main
+
+import android.os.Bundle
+import android.view.View
+import com.app.eco_co.common.util.BaseFragment
+import com.app.eco_co.databinding.FragmentArticleHomeBinding
+
+
+private const val TAG = "ArticleHomeFragment_창영"
+
+class ArticleHomeFragment :
+    BaseFragment<FragmentArticleHomeBinding>(FragmentArticleHomeBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+} // End of ArticleHomeFragment class
